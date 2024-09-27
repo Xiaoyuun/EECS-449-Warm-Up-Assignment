5948bdd559e4d19f7e63ef56fd5923f8bc59b617
Julia Ruan <62456112+Xiaoyuun@users.noreply.github.com>
2024-09-27 11:26:55 -0400

    add part 1 screenshots
    - add screenshots for part 1: swagger documentation of new walkers and responses from API when calling new walkers

---
+++ __jac_gen__/server.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+from jaclang.plugin.feature import JacFeature as _Jac
+from jaclang.plugin.builtin import *
+from dataclasses import dataclass as __jac_dataclass__
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact(_Jac.Walker):
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, world!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class interact_with_body(_Jac.Walker):
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hello, ' + self.name + '!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class walker_one(_Jac.Walker):
+    amount: int
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'There are ' + self.amount + ' apples!'})
+
+@_Jac.make_walker(on_entry=[_Jac.DSFunc('return_message', _Jac.RootType)], on_exit=[])
+@__jac_dataclass__(eq=False)
+class walker_two(_Jac.Walker):
+    amount: int
+    name: str
+
+    def return_message(self, _jac_here_: _Jac.RootType) -> None:
+        _Jac.report({'response': 'Hi ' + self.name + '! There are ' + self.amount + ' penguins outside!'})
